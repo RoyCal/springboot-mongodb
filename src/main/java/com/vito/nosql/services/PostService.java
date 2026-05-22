fc3b8fac5f774ea567bdea327f6dff0ea6c98337
RoyCal <vito.elias9@gmail.com>
2026-05-22 16:24:42 -0300

    Obtendo um post por id

---
+++ src/main/java/com/vito/nosql/services/PostService.java
@@ -0,0 +1,23 @@
+package com.vito.nosql.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vito.nosql.domain.Post;
+import com.vito.nosql.repositories.PostRepository;
+import com.vito.nosql.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
