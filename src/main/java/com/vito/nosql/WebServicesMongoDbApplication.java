12d101b5b924d2e1fc48f92a32ffbc30acda95fb
RoyCal <vito.elias9@gmail.com>
2026-05-22 09:49:54 -0300

    Project created

---
+++ src/main/java/com/vito/nosql/WebServicesMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.vito.nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vito/nosql/WebServicesMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vito.nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
